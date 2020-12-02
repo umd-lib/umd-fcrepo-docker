8f205f8dcd2f17605a721889000e87c1ef5084b5
Peter Eichman <peichman@umd.edu>
2020-12-02 14:47:03 -0500

    LIBFCREPO-930. Give archelon user createdb privileges.
    https://issues.umd.edu/browse/LIBFCREPO-930

--- postgres-archelon/init-archelon-db.sh
+++ postgres-archelon/init-archelon-db.sh
@@ -3,7 +3,7 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<END
 CREATE DATABASE archelon;
 
-CREATE USER archelon WITH PASSWORD 'archelon';
+CREATE USER archelon WITH CREATEDB PASSWORD 'archelon';
 
 GRANT ALL PRIVILEGES ON DATABASE archelon TO archelon;
 END
